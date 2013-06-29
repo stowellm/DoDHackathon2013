69a5961731f4ef59ef710f9517e92c0734b7f0f2
stowellm <stowellmichael@gmail.com>
2013-06-29 03:07:13 -0400

    Added picker_food layout

---
+++ DoDHackathon2013/src/dod/hackathon/combatfeeding/FoodPicker.java
@@ -0,0 +1,17 @@
+package dod.hackathon.combatfeeding;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodPicker extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.picker_food);
+		
+		
+		
+	}
+	
+}
